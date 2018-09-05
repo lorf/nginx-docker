70bd5a438c582565f12e94fd22c20c791049f3b0
Dmitry Frolov <dvfrolov@mts.ru>
2018-09-05 14:15:29 +0700

    nginx-unprivileged-extra: add

---
+++ nginx-unprivileged-extra/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -xe
+
+while :; do
+    ffmpeg -v warning -re -stream_loop -1 -i /usr/share/nginx/rtmp/testloop/big_buck_bunny_480p_1mb.mp4 -threads 2 -vcodec libx264 -vprofile baseline -acodec aac -ar 44100 -ab 256k -r 25 -s 640x360 -f flv -strict -2 rtmp://localhost/hls/test || true
+done &
+
+while :; do
+    ffmpeg -v warning -re -stream_loop -1 -i /usr/share/nginx/rtmp/testloop/big_buck_bunny_480p_1mb.mp4 -threads 2 -vcodec libx264 -vprofile baseline -acodec aac -ar 44100 -ab 256k -r 25 -s 640x360 -f flv -strict -2 rtmp://localhost/dash/test || true
+done &
+
+nginx -g 'daemon off;'
