d3fddff6d2829fd42a676de6a55702cdaa7c917e
Dmitry Frolov <dvfrolov@mts.ru>
2018-11-27 11:54:48 +0700

    Change sample video

--- nginx-unprivileged-extra/run.sh
+++ nginx-unprivileged-extra/run.sh
@@ -3,11 +3,11 @@
 set -xe
 
 while :; do
-    ffmpeg -v warning -re -stream_loop -1 -i /usr/share/nginx/rtmp/testloop/big_buck_bunny_480p_1mb.mp4 -threads 2 -vcodec libx264 -vprofile baseline -acodec aac -ar 44100 -ab 256k -r 25 -s 640x360 -f flv -strict -2 rtmp://localhost/hls/test || true
+    ffmpeg -v warning -re -stream_loop -1 -i /usr/share/nginx/rtmp/testloop/small.mp4 -threads 2 -vcodec libx264 -vprofile baseline -acodec aac -ar 44100 -ab 256k -r 25 -s 640x360 -f flv -strict -2 rtmp://localhost/hls/test || true
 done &
 
 while :; do
-    ffmpeg -v warning -re -stream_loop -1 -i /usr/share/nginx/rtmp/testloop/big_buck_bunny_480p_1mb.mp4 -threads 2 -vcodec libx264 -vprofile baseline -acodec aac -ar 44100 -ab 256k -r 25 -s 640x360 -f flv -strict -2 rtmp://localhost/dash/test || true
+    ffmpeg -v warning -re -stream_loop -1 -i /usr/share/nginx/rtmp/testloop/small.mp4 -threads 2 -vcodec libx264 -vprofile baseline -acodec aac -ar 44100 -ab 256k -r 25 -s 640x360 -f flv -strict -2 rtmp://localhost/dash/test || true
 done &
 
 nginx -g 'daemon off;'
